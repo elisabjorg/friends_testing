3f87fee66908ebc007855702d789aa12a681210a
Elisa Bjorg Sveinsdottir <elisabjorg22@gmail.com>
2019-01-17 15:17:33 +0000

    Test six passed

--- specs/friends_spec.rb
+++ specs/friends_spec.rb
@@ -99,11 +99,17 @@ end
 
   # 5. For a given person, remove a specific name from their list of friends
   # (hint: Same as above, testing for the length of the array should be sufficient)
-
+def test_remove_friend
+  result = get_remove_friend(@person3, [:friends])
+  assert_equal(5, result)
+end
 
   # 6. Find the total of everyone's money
   # (hint: use the @people array, remember how we checked the total number of eggs yesterday?)
-
+def test_total_money
+  result = get_total_money(@people, [:monies])
+  assert_equal(143, result)
+end
 
   # 7. For two given people, allow the first person to loan a given value of money to the other
   # (hint: our function will probably need 3 arguments passed to it... the lender, the lendee, and the amount for this function)
